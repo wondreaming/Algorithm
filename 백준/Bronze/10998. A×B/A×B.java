bcd3d427fe6a2434dd685bf0eaab7d991c6e8713
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-01-03 22:15:02 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = bufferedReader.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+
+        int result = A * B;
+        System.out.println(result);
+        
+        bufferedReader.close();
+    }
+}
